1b5e29ba2e7d63895ded4a28b46737d7fb883ef8
Taylor Doodchenko <taylordoodchenko@Taylors-MacBook-Pro.local>
2018-12-10 23:24:49 -0500

    app function. needs heroku deployment

--- app/data/app.js
+++ app/data/app.js
@@ -1,81 +1,81 @@
 var friends = [
     {
       name: "Brian Griffin",
-      photo: "http://media.comicbook.com/2018/06/family-guy-brian-1118520.jpeg",
+      photo: "https://github.com/tdoodchenko/FriendFinder/blob/master/app/assets/images/brian-G.jpeg?raw=true",
       scores: [
-        "5",
-        "1",
-        "1",
-        "5",
         "1",
         "5",
         "5",
-        "2",
+        "5",
+        "5",
+        "5",
+        "4",
         "1",
+        "5",
         "3"
       ]
     },
     {
       name: "Chris Griffin",
-      photo: "https://tv-fanatic-res.cloudinary.com/iu/s--r8bnJLbP--/c_scale,f_auto,h_522,q_auto,w_696/v1371163248/chris-as-luke-skywalker",
+      photo: "https://github.com/tdoodchenko/FriendFinder/blob/master/app/assets/images/chris-G.jpg?raw=true",
       scores: [
         "5",
+        "4",
         "2",
-        "3",
-        "5",
-        "3",
+        "1",
+        "2",
         "4",
         "5",
         "3",
         "4",
-        "3"
+        "1"
       ]
     },
     {
       name: "Meg",
-      photo: "https://pbs.twimg.com/media/BqENoQeCcAEbjTp.jpg",
+      photo: "https://github.com/tdoodchenko/FriendFinder/blob/master/app/assets/images/Meg.jpg?raw=true",
       scores: [
-        "5",
+        "1",
+        "1",
+        "1",
         "3",
-        "5",
         "3",
-        "2",
-        "5",
-        "5",
-        "3",
-        "5",
-        "3"
-      ]
-    },
-    {
-      name: "Glenn Quagmire",
-      photo: "https://cdn.jwplayer.com/thumbs/UaM62ZBW-720.jpg",
-      scores: [
-        "5",
-        "2",
         "1",
         "5",
-        "3",
-        "5",
         "4",
-        "2",
         "3",
         "5"
       ]
     },
     {
-      name: "Herbert",
-      photo: "https://i.ytimg.com/vi/y41vObbUf9k/maxresdefault.jpg",
+      name: "Glenn Quagmire",
+      photo: "https://github.com/tdoodchenko/FriendFinder/blob/master/app/assets/images/glenn-Q.png?raw=true",
       scores: [
+        "3",
+        "4",
+        "3",
+        "3",
+        "4",
+        "2",
+        "3",
+        "3",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: "Herbert",
+      photo: "https://github.com/tdoodchenko/FriendFinder/blob/master/app/assets/images/herbert.jpg?raw=true",
+      scores: [
+        "3",
+        "3",
+        "1",
         "5",
         "2",
         "1",
+        "3",
         "4",
-        "5",
-        "5",
-        "5",
-        "2",
-        "2",
+        "3",
         "4"
       ] 
     }
